67d95c17dba548af9cee5146f2570e1ce87aa24c
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2020-04-27 19:02:03 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Google Data Studio
+
+### Description
+
+Displays [Data Studio](https://datastudio.google.com/) dashboard in WordPress administration panel.
---
+++ wp-google-datastudio.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Google Data Studio
+ * Description: Displays Data Studio dashboard in WordPress administration panel.
+ * Plugin URI: https://github.com/innocode-digital/wp-google-datastudio
+ * Version: 0.0.1
+ * Author: Innocode
+ * Author URI: https://innocode.com
+ * Tested up to: 5.4
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
